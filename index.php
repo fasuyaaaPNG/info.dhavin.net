6c2b7448cdee70b36c7d4d632883e21b3793f132
younix <154081073+fasuyaaaPNG@users.noreply.github.com>
2024-11-12 07:25:01 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sixtyfour+Convergence&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Sixtyfour+Convergence&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sixtyfour+Convergence&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <div class="isiHeader">
+            <p class="logo">
+                Dhavin.net
+            </p>
+            <a href="http://dhavin.net/" class="home">
+                Home
+            </a>
+        </div>
+    </div>
+    <div class="isi1">
+        <div class="isiIsi1">
+            <div class="sisiKiri1">
+                <p class="judulSisiKiri1">
+                    Selamat Datang di info.dhavin.net
+                </p>
+                <p class="deskSisiKiri1">
+                    Actus me invito factus non est meus actus.
+                </p>
+                <a href="" class="button">
+                    View more
+                </a>
+            </div>
+            <div class="sisiKanan1">
+                //<img class="gambarNolan" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fbcc8e98-1e32-4651-9919-367525a4ebf7/di8i8ac-034a99b9-00cb-4891-897c-f998c7b22248.png/v1/fill/w_1280,h_720/nolan_the_navigator_11_11__render_png_by_wolvesdzn_di8i8ac-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZmJjYzhlOTgtMWUzMi00NjUxLTk5MTktMzY3NTI1YTRlYmY3XC9kaThpOGFjLTAzNGE5OWI5LTAwY2ItNDg5MS04OTdjLWY5OThjN2IyMjI0OC5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.xTsB_BCb1_RpnDHJmuBSA3DU4xa2cWGvNAgAa9EKWdE" alt="" class="nolan">
+            </div>
+        </div>
+    </div>
+    <div class="isi2">
+        <p class="isi2Kiri">
+            Siapa Kami?
+        </p>
+        <p class="isi2Kanan">
+        Kami adalah tim ilustrator kreatif yang siap mewujudkan karakter impianmu. Baik itu karakter untuk game, komik, novel, atau sekadar hobi, kami akan buat dengan desain yang unik dan menarik.
+        </p>
+    </div>
+    <div class="isi3">
+        <p class="isi3Kiri">
+            Layanan Utama
+        </p>
+        <p class="isi3Kanan">
+        Konsultasi Desain: Diskusi mendalam mengenai konsep dan gaya karakter.<br>
+Modeling 3D:
+Pembuatan model 3D sesuai dengan spesifikasi dan desain yang telah disepakati.<br>
+Tekstur dan Material:
+Penerapan tekstur yang realistis atau stylized sesuai kebutuhan<br>
+Rigging dan Animasi (opsional):
+Menyediakan rigging untuk karakter agar dapat dianimasikan.<br>
+Render dan Presentasi:
+Render karakter dalam berbagai pose dan pencahayaan.
+        </p>
+    </div>
+    <div class="isi4">
+        <p class="isi4Kiri">
+            Mengapa Memilih Kami?
+        </p>
+        <p class="isi4Kanan">
+        - Tim ahli dan berpengalaman. <br>
+        - Solusi yang disesuaikan dengan kebutuhan. <br>
+        - Layanan pelanggan yang ramah dan cepattanggap. <br>
+        </p>
+    </div>
+    <div class="footer">
+        <p class="isiffKiri">
+            Hubungi Kami
+        </p>
+        <div class="isif">
+            <p class="isi4Kiri">
+                Hubungi kami melalui
+            </p>
+            <p class="isi4Kanan">
+            - Email: dhavin@dhavin.net <br>
+            - WA: 085700489689 <br>
+            - Alamat: Jalan Pekunden Selatan <br>
+            </p>
+        </div>
+        <p class="isifKiri">
+            Kami senang dapat membantu Anda. Terima kasih telah mengunjungi halaman informasi kami!
+        </p>
+    </div>
+</body>
+</html>
